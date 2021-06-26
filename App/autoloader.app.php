13e5b9734b933e14602e47310129041f008193a9
Ken Terrado <79209076+rterrado@users.noreply.github.com>
2021-06-26 15:25:59 +0800

    creating autoloader

---
+++ App/autoloader.app.php
@@ -0,0 +1,13 @@
+<?php
+
+spl_autoload_register ( 'autoloader' );
+
+function autoloader ( string $className ) {
+  
+    $path = $_SERVER["DOCUMENT_ROOT"] . "/" . str_replace('\\', '/', $className) . ".php";
+  
+    if ( file_exists ( $path ) ) {
+        require_once $path;
+    }
+  
+}
